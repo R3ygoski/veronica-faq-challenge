2b236c43b1e41824760b6b124a9172970190647b
Bernardo Poggioni <hckranonymous2@hotmail.com>
2024-09-14 16:00:27 -0300

    Primeiro Commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const buttons = document.querySelectorAll("[data-btn-id=btn]");
+
+buttons.forEach(button => {
+    button.addEventListener("click", handleClick);
+});
+
+function handleClick(e) {
+    const btnClicked = e.target;
+    const img = btnClicked.querySelector("img")
+
+    if(img.getAttribute("src") == "images/icon-plus.svg"){
+        img.setAttribute("src","images/icon-minus.svg")
+    } else {
+        img.setAttribute("src","images/icon-plus.svg")
+    }
+
+    const btnParent = btnClicked.parentNode;
+    const reply = btnParent.querySelector("[data-reply]");
+
+    reply.classList.toggle("hide");
+}
